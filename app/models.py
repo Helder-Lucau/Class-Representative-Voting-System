e103bb25124c8bc5acd5a9e8f7286c8a2677488c
Helder-Lucau <helderlucau@gmail.com>
2023-09-07 23:24:43 +0300

    repr function to return a printable representation of the object

--- app/models.py
+++ app/models.py
@@ -13,7 +13,14 @@ class Voter(Base):
     first_name = Column(String(), nullable=False)
     last_name = Column(String(), nullable=False)
     phone = Column(Integer) 
-    course = Column(String()) 
+    course = Column(String())
+
+    def __repr__(self):
+        return f"review{self.id}: " \
+            + f"first_name = {self.first_name}, " \
+            + f"last_name = {self.last_name}, " \
+            + f"phone = {self.phone}, " \
+            + f"course = {self.course}" 
 
 class Candidate(Base):
     __tablename__ = "candidates"
@@ -24,11 +31,23 @@ class Candidate(Base):
     course = Column(String())
     party = Column(String())
 
-class Votes(Base):
+    def __repr__(self):
+        return f"review{self.id}: " \
+            + f"first_name = {self.first_name}, " \
+            + f"last_name = {self.last_name}, " \
+            + f"course = {self.course}, " \
+            + f"party = {self.party}" 
+
+class Vote(Base):
     __tablename__ = "votes"
 
     id = Column(Integer, primary_key=True)
     voter_id = Column(Integer, ForeignKey('voters.id'))
     candidate_id = Column(Integer, ForeignKey('canditates.id'))
+    vote_count = Column(Integer())
 
-    
+    def __repr__(self):
+        return f"review{self.id}: " \
+            + f"voter id = {self.voter_id}, " \
+            + f"candidate id = {self.candidate_id}, " \
+            + f"number of vote = {self.vote_count}" 
